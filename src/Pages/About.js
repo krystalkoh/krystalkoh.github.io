fa92defe91c7366430fb2875224ca0360b67b9ef
Krystal <krystalkoh@gmail.com>
2022-08-26 00:42:57 +0800

    About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -49,42 +49,38 @@ const About = () => {
               </Grid>
             </Grid>
             <br></br>
-            <Text>
-              {" "}
-              <h3> Why the change in career?</h3>I used to be a teacher. Due to
-              the onset of the pandemic, I found myself using technology to
-              teach on a daily basis. Creating innovative technology-enabled
-              lessons brought me back to my teenage days where I customised
-              blogskins by editing its HTML and CSS. I decided to take up a
-              basic coding course while I was teaching and the feeling of
-              accomplishment from creating a website from scratch was
-              incredible, and I knew that was going to be the career that I
-              truly wanted to pursue. <br />I decided to enroll in the full-time
+            <Text variant="h4" sx={{ color: "#5C3C3D", mt: 3 }}>
+              Why the change in career?
+            </Text>
+            <Text sx={{ mt: 3 }}>
+              I used to be a teacher. Due to the onset of the pandemic, I found
+              myself using technology to teach on a daily basis. Creating
+              innovative technology-enabled lessons brought me back to my
+              teenage days where I customised blogskins by editing its HTML and
+              CSS. I decided to take up a basic coding course while I was
+              teaching and the feeling of accomplishment from creating a website
+              from scratch was incredible, and I knew that was going to be the
+              career that I truly wanted to pursue. <br />I decided to enroll in
+              the full-time
               <b>Software Engineering Immersive </b>programme with General
               Assembly and it has been the best decision I’ve made yet.
             </Text>
           </Box>
         </Grid>
+
         <Grid item xs={6} sx={{ backgroundColor: "#9bad98" }}>
-          <Box
-            sx={{
-              my: 20,
-              height: "100%",
-              width: "90%",
-              mx: 5,
-            }}
-          >
-            <Text sx={{ color: "#5C3C3D" }}>
-              <h3> Why hire me?</h3>I am an enthusiastic, adaptable individual
-              who believes in active learning. My science background and my
-              creative side puts me in good stead to excel if given an
-              opportunity to do so. Through my various leadership roles and
-              collaboration with various stakeholders, it has honed me to be a
-              good teammate and leader. I'm a keen learner who is always ready
-              to take on a challenge.
-            </Text>{" "}
-            <br></br>
-          </Box>
+          <Text sx={{ color: "#5C3C3D", mt: 25 }} variant="h3">
+            Why hire me?
+          </Text>
+          <Text variant="h6" sx={{ mt: 5, px: 10 }}>
+            I am an enthusiastic, adaptable individual who believes in active
+            learning. My science background and my creative side puts me in good
+            stead to excel if given an opportunity to do so. Through my various
+            leadership roles and collaboration with various stakeholders, it has
+            honed me to be a good teammate and leader. I'm a keen learner who is
+            always ready to take on a challenge.
+          </Text>{" "}
+          <br></br>
         </Grid>
       </Grid>
     </div>
